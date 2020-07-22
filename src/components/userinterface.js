5d4c93b065d507d7799e6629cee84b84e0d90656
szkmk13 <szkmk131@gmail.com>
2020-07-22 09:40:34 +0200

    Dodanie systemu ataku

--- src/components/userinterface.js
+++ src/components/userinterface.js
@@ -5,17 +5,54 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Ashe from "../images/Ashe.jpg";
 import Zoe from "../images/Zoe.jpg";
 
+
 class UserInterface extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hp: 10,
+            lvl:1,
+            dmgOnClick:1,
+
+        };
+        // Poniższe wiązanie jest niezbędne do prawidłowego przekazania this przy wywołaniu funkcji
+        this.dealDmg = this.dealDmg.bind(this);
+    }
+    dmgOnClick = () => {
+        let { dmgOnClick, lvl} = this.state;
+        dmgOnClick=lvl*21;
+        this.setState({dmgOnClick});
+    }
+    dealDmg = () => {
+        let { hp , dmgOnClick, lvl} = this.state;
+        hp = hp - lvl*2;
+        this.setState({dmgOnClick, hp});
+    }
+
+
+
     render() {
         return (
             <Container>
                 <Row>
-                    <Col><div className="playerBox">Gracz</div></Col>
-                    <Col><div className="playerBox">Przeciwknik</div></Col>
+                    <Col><div className="goldbox">Gold:</div><div>wartocs nwm jak </div></Col>
+                    <Col><div className="dmgbox">Dmg/click:</div><div>{this.state.dmgOnClick}</div></Col>
+                    <Col><div className="dmgbox">Dmg/s:</div><div>wartocs nwm jak </div></Col>
                 </Row>
                 <Row>
                     <Col><img className="champion" src={Zoe} alt="Zoe" /></Col>
-                    <Col><img className="champion" src={Ashe} alt="Ashe" /></Col>
+                    <Col></Col>
+                    <Col>
+                        <div className="enemy">
+                            <button className="attackbutton" onClick={this.dealDmg}>
+                                \* <img src={Ashe} alt="Ashe" />  */
+                            </button>
+                           <div>{this.state.hp}</div>
+                        </div>
+                        <div>
+
+                        </div>
+                    </Col>
                 </Row>
             </Container>
         );
